65b1c9cdb7ddf501a65a3573b20ae51df99c71e1
wyn-cmd <80321275+wyn-cmd@users.noreply.github.com>
2021-08-11 17:04:53 +0800

    Update IDE_code.ino

--- IDE_code.ino
+++ IDE_code.ino
@@ -3,10 +3,10 @@ Servo myservo;
 
 void setup() {
   int pos=0;
-  pinMode(2,INPUT);
-  pinMode(7,INPUT);
+  pinMode(2,INPUT);              // exit pressure-plate
+  pinMode(7,INPUT);              // enter pressure-plate
   pinMode(9,OUTPUT);
-  myservo.attach(11);            //servo 1
+  myservo.attach(11);            // servo to block or allow the door to open
   Serial.begin(2000000);
   for (pos=180;pos>=0;pos-=1) {  // goes from 180 degrees to 0 degrees
     myservo.write(pos);          // tell servo to go to position in variable pos
@@ -14,11 +14,11 @@ void setup() {
   myservo.write(90);   
 }
 int person_enter=0;
-int max=3;                       //The limit of people allowed in the area
+int max=3;                       // the limit of people allowed in the area
 void loop() {
 	int pos=0;
 	int d=14;
-  Serial.println("---start---");
+  Serial.println("---start---"); // serial monitor to check if it is working
   Serial.println("")
   Serial.print("Digital Pin 7 (RED) (ENTER):  ");
   Serial.println(digitalRead(7));
@@ -35,17 +35,17 @@ void loop() {
     person_enter=0;
   };
 	if(digitalRead(7)==1){
-    person_enter=person_enter+1; //someone entered
+    person_enter=person_enter+1; // someone entered
     delay(600);
-    if(person_enter>max-1){      //Checks if reached limit of people allowed in the area
+    if(person_enter>max-1){      // checks if reached limit of people allowed in the area
       myservo.write(0);
       delay(d);
     };
 	};
   if(digitalRead(2)==1){
-    person_enter=person_enter-1; //someone left
+    person_enter=person_enter-1; // someone left
     delay(600);
-    if(person_enter<max){        //Checks if below the limit of people allowed in the area
+    if(person_enter<max){        // checks if below the limit of people allowed in the area
       myservo.write(90);
       delay(d);
     };
