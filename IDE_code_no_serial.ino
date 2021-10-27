5d52e1966c82654ea792001fdc9c659fb4ddba8c
wyn-cmd <80321275+wyn-cmd@users.noreply.github.com>
2021-10-27 13:28:09 +0800

    Update IDE_code_no_serial.ino

--- IDE_code_no_serial.ino
+++ IDE_code_no_serial.ino
@@ -3,42 +3,48 @@ Servo myservo;
 
 void setup() {
   int pos=0;
-  pinMode(2,INPUT);              // exit pressure-plate
-  pinMode(7,INPUT);              // enter pressure-plate
+  // exit pressure-plate
+  pinMode(2,INPUT);
+  // enter pressure-plate
+  pinMode(7,INPUT);
   pinMode(9,OUTPUT);
-  myservo.attach(11);            // servo to block or allow the door to open
-  for (pos=180;pos>=0;pos-=1) {  // goes from 180 degrees to 0 degrees
-    myservo.write(pos);          // tell servo to go to position in variable pos
+  // servo to block or allow the door to open
+  myservo.attach(11);
+  // goes from 180 degrees to 0 degrees
+  for (pos=180;pos>=0;pos-=1) {
+    // tell servo to go to position in variable pos
+    myservo.write(pos);
   };
   myservo.write(90);   
-}
+};
 int person_enter=0;
-int max=3;                       // the limit of people allowed in the area
+// limit of people allowed in the area
+int max=3;
 void loop() {
-  int pos=0;
-  int d=14;
+	int pos=0;
+	int d=14;
   delay(5);
   if(person_enter<0){
     person_enter=0;
   };
   if(digitalRead(7)==1){
-    person_enter=person_enter+1; // someone entered
+    // someone entered
+    person_enter=person_enter+1; 
     delay(600);
-    if(person_enter>max-1){      // checks if reached limit of people allowed in the area
+    // checks if reached limit of people allowed in the area
+    if(person_enter>max-1){      
       myservo.write(0);
       delay(d);
     };
 	};
   if(digitalRead(2)==1){
-    person_enter=person_enter-1; // someone left
+    // someone left
+    person_enter=person_enter-1; 
     delay(600);
-    if(person_enter<max){        // checks if below the limit of people allowed in the area
+    // checks if below the limit of people allowed in the area
+    if(person_enter<max){
       myservo.write(90);
       delay(d);
     };
   };
 }
-
-
-
-
